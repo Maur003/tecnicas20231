7b774d3ba3e5ea8508c4b6a1d43a37ed919e57ec
Daniel Pareja Londoño <dpareja1394@gmail.com>
2023-03-19 14:02:46 -0500

    Agregar Modelo Consignacion y Retiro

--- bancaweb/bancaweb/src/main/java/co/edu/usbcali/bancaweb/model/Consignacion.java
+++ bancaweb/bancaweb/src/main/java/co/edu/usbcali/bancaweb/model/Consignacion.java
@@ -1,4 +1,32 @@
 package co.edu.usbcali.bancaweb.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
 public class Consignacion {
+    @Id
+    @Column(name = "con_codigo", length = 10, nullable = false)
+    private Integer codigo;
+
+    @ManyToOne
+    @JoinColumn(name = "cue_numero", referencedColumnName = "cue_numero")
+    private Cuenta cuenta;
+
+    @ManyToOne
+    @JoinColumn(name = "usu_cedula", referencedColumnName = "usu_cedula")
+    private Usuario usuario;
+
+    @Column(name = "con_valor", length = 10, precision = 2, nullable = false)
+    private BigDecimal valor;
+
+    @Column(name = "con_fecha", nullable = false)
+    private Timestamp fecha;
+
+    @Column(name = "con_descripcion", length = 50, nullable = false)
+    private String descripcion;
 }
