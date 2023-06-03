73992037d4e38c6abf04a63e15bfa8f116204787
Daniel Pareja Londoño <dpareja1394@gmail.com>
2023-06-03 14:58:12 -0500

    Repositorio para la entidad cliente

--- tienda/tiendaapp/src/main/java/co/edu/usbcali/tiendaapp/repository/ClienteRepository.java
+++ tienda/tiendaapp/src/main/java/co/edu/usbcali/tiendaapp/repository/ClienteRepository.java
@@ -1,4 +1,9 @@
 package co.edu.usbcali.tiendaapp.repository;
 
-public interface ClienteRepository {
+import co.edu.usbcali.tiendaapp.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 }
