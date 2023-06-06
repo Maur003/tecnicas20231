4631cf5f5812af507a9a8f0af45293555e329e9b
Daniel Pareja Londoño <dpareja1394@gmail.com>
2023-06-06 14:13:50 -0500

    Creación de Repository para todas las entidades

--- tienda/tiendaapp/src/main/java/co/edu/usbcali/tiendaapp/repository/ProductoRepository.java
+++ tienda/tiendaapp/src/main/java/co/edu/usbcali/tiendaapp/repository/ProductoRepository.java
@@ -1,9 +1,9 @@
 package co.edu.usbcali.tiendaapp.repository;
 
-import co.edu.usbcali.tiendaapp.domain.Categoria;
+import co.edu.usbcali.tiendaapp.domain.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 }
