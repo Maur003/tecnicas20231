9102e3eba96866134b5f6c0bd0656793ff3a440f
Daniel Pareja Londoño <dpareja1394@gmail.com>
2023-06-09 20:33:30 -0500

    Service

--- tienda/tiendaapp/src/main/java/co/edu/usbcali/tiendaapp/service/ClienteService.java
+++ tienda/tiendaapp/src/main/java/co/edu/usbcali/tiendaapp/service/ClienteService.java
@@ -1,4 +1,13 @@
 package co.edu.usbcali.tiendaapp.service;
 
+import co.edu.usbcali.tiendaapp.dto.ClienteDTO;
+
+import java.util.List;
+
 public interface ClienteService {
+    List<ClienteDTO> obtenerTodos();
+    ClienteDTO buscarPorId(Integer id);
+    ClienteDTO guardar(ClienteDTO clienteDTO);
+    ClienteDTO actualizar(ClienteDTO clienteDTO);
+
 }
