14351ceb525bcc2d6b697d3508a95318c82fc07a
Daniel Pareja Londoño <dpareja1394@gmail.com>
2023-06-20 19:06:13 -0500

    Agregar dependencias para Swagger

--- tienda/tiendaapp/src/main/java/co/edu/usbcali/tiendaapp/controller/CategoriaController.java
+++ tienda/tiendaapp/src/main/java/co/edu/usbcali/tiendaapp/controller/CategoriaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categoria")
+@RequestMapping(value = "/categoria")
 public class CategoriaController {
 
     private final CategoriaService categoriaService;
@@ -16,22 +16,22 @@ public class CategoriaController {
         this.categoriaService = categoriaService;
     }
 
-    @PostMapping("/nueva")
+    @PostMapping(value = "/nueva")
     CategoriaDTO nuevaCategoria(@RequestBody CategoriaDTO categoriaDTO) throws Exception {
         return categoriaService.guardar(categoriaDTO);
     }
 
-    @GetMapping("/buscarTodas")
+    @GetMapping(value = "/buscarTodas")
     List<CategoriaDTO> buscarTodas() {
         return categoriaService.obtenerTodos();
     }
 
-    @PutMapping("/actualizar")
+    @PutMapping(value = "/actualizar")
     CategoriaDTO actualizarCategoria(@RequestBody CategoriaDTO categoriaDTO) throws Exception {
         return categoriaService.actualizar(categoriaDTO);
     }
 
-    @GetMapping("/buscarPorNombre/")
+    @GetMapping(value = "/buscarPorNombre/")
     List<CategoriaDTO> buscarPorNombre(@RequestParam("nombre") String nombre) throws Exception {
         return categoriaService.buscarPorNombreLike(nombre);
     }
